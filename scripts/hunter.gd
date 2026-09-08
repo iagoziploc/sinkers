ef5898a1a4d66abc0ecb956fa8ec032aa71e2b03
iago <iagoscippo@gmail.com>
2026-09-08 18:19:31 -0300

    hola

--- scripts/hunter.gd
+++ scripts/hunter.gd
@@ -6,14 +6,32 @@ extends CharacterBody2D
 @export var hunt_interval := 10.0
 var direccion := Vector2.from_angle(randf_range(0, TAU))
 var tiempo : float
+var objetivo = null
 
 
 func _physics_process(delta: float) -> void:
 	if tiempo >= hunt_interval:
-		direccion = Vector2.from_angle(randf_range(0, TAU))
-		print("cambio direccion")
+		var alive_floaters = get_tree().get_nodes_in_group("floater")
+		if not alive_floaters.is_empty():
+			var mas_cercano = null
+			var menor_distancia = INF
+			for floater in alive_floaters:
+				var dist = position.distance_to(floater.position)
+				if dist < menor_distancia:
+					menor_distancia = dist
+					mas_cercano = floater
+			objetivo = mas_cercano
+			print("obj encontrado")
+		else: 
+			objetivo = null
+			print("cambio direccion")
 		tiempo = 0.0
+		if objetivo != null and is_instance_valid(objetivo):
+			direccion = (objetivo.position - position).normalized()
+			velocity = direccion * target_speed
+		else:
+			direccion = Vector2.from_angle(randf_range(0, TAU))
+			velocity = direccion * idle_speed
 
 	tiempo += delta
-	velocity = direccion * idle_speed
 	move_and_slide()
